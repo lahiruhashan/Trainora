696dd89c7de0b010f31873a0dc4eff59eac5d588
Tines Kumar <91739893+Tines-cloud@users.noreply.github.com>
2025-05-01 22:00:29 +0800

    insert sample exercise data
    Sample data insert code writing

--- Trainora/TrainoraApp.swift
+++ Trainora/TrainoraApp.swift
@@ -12,6 +12,25 @@ struct TrainoraApp: App {
     let persistenceController = PersistenceController.shared
 
     @StateObject private var appState = AppState()
+    
+    // code for sample data insert.
+    /*
+    init() {
+        let context = persistenceController.container.viewContext
+        let seeder = SampleDataSeeder(context: context)
+
+        
+            if let url = Bundle.main.url(forResource: "beginner_sample_data", withExtension: "json"),
+               let data = try? Data(contentsOf: url),
+               let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+               let categories = json["categories"] as? [[String: Any]],
+               let exercises = json["exercises"] as? [[String: Any]] {
+                
+                seeder.seed(categories: categories, exercises: exercises)
+                UserDefaults.standard.set(true, forKey: "didSeedSampleData")
+            }
+    }
+     */
 
     var body: some Scene {
         WindowGroup {
